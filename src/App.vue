55c447b6492f8b15c0d55234d7b73734e6c89f04
Jonathan Sch <jonathan.schaeberle@outlook.de>
2020-08-24 23:25:45 +0200

    Bug Require :key in template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <template v-for="todo in todos">
+        <li :key="todo.text">{{ todo.text }}</li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => {
+    return {
+      todos: [
+        { text: "Learn JavaScript", done: false },
+        { text: "Learn Vue", done: false },
+        { text: "Build something awesome", done: true },
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
